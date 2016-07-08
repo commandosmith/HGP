19f24a18b7c699c329fc91507d53eafdfa1477f0
Quentin Smith <quentinsmith@Thes-MacBook-Pro.local>
2016-07-08 11:25:02 -0700

    Tie Fighter

--- hiddenGeniusProject/ViewController.swift
+++ hiddenGeniusProject/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var explodingTie: UIImageView!
+    @IBOutlet weak var tieFighter: UIImageView!
+    @IBAction func shootButton(sender: AnyObject) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func shootFighter(sender: AnyObject) {
+        tieFighter.hidden = true
+    
+    }
+ 
 }
 
